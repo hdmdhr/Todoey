8bb64921557b29b9b178ca93ce0459fc2b188388
DongMing <MacBookFrog@node-1w7jra204366mpm7g04wn6b74.ipv6.telus.net>
2018-05-03 16:53:27 -0600

    To do list can be checked, not persistence.

--- Todoey/ViewController.swift
+++ Todoey/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class TodoListVC: UITableViewController {
+    
+    let placeHolder = ["buy eggs", "eat meals", "take showers"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //MARK: - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeHolder.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        cell.textLabel?.text = placeHolder[indexPath.row]
+        
+        return cell
+    }
 
+    // MARK: - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)   // 选中后一闪而过
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
     
 
 }
